8ae1ae908e88c76a510147212eb87787aac5a6f1
Ronlewis483 <ronlewis483@gmail.com>
2026-09-25 14:21:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6182,8 +6182,8 @@ if mlb_metrics:
 
 
 
-        except Exception as e:
-            st.error(
-                f"MLB model training failed: {e}"
-            )
-            st.exception(e)
+except Exception as e:
+    st.error(
+        f"MLB model training failed: {e}"
+    )
+    st.exception(e)
